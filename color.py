a36c2f14006d0fe15fb80d68993e8fb9d09dac7b
MaxBuster <maxbuster380@gmail.com>
2023-01-07 18:42:01 +0100

    Added Color Helper

--- color.py
+++ color.py
@@ -9,3 +9,14 @@ def green(color):
 def blue(color):
     """Returns the blue component of the color"""
     return color[2]
+
+def color_distance(c1, c2):
+    """Returns the distance between two colors"""
+    r1 = red(c1)
+    g1 = red(c1)
+    b1 = red(c1)
+    r2 = red(c2)
+    g2 = red(c2)
+    b2 = red(c2)
+
+    return ((r1-r2)**2 + (g1-g2)**2 + (b1-b2)**2)**0.5
